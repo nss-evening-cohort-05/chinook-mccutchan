2ad02d61c42da2a6f031249cb1d7b42d25b934b8
John McCutchan <john.mccutchan.wd@gmail.com>
2017-08-07 20:45:37 -0500

    Failing query

--- sales_agent_invoices.sql
+++ sales_agent_invoices.sql
@@ -1,3 +1,14 @@
 /*sales_agent_invoices.sql: Provide a query that shows the invoices associated with each sales agent. 
 The resultant table should include the Sales Agent's full name.*/
 
+/*Map*/
+SELECT  e.Title, e.FirstName, e.LastName, i.InvoiceId
+
+/*What tables*/
+FROM Invoice i 
+	JOIN Customer c
+		USING i.CustomerId, c.CustomerId
+	JOIN Employee e
+		USING e.EmployeeId, c.SupportRepId
+
+/*WHERE e.Title = "Sales Support Agent"*/
