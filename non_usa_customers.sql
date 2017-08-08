be97552c4cb56c143cdec69ad27bd75e0606f0e6
John McCutchan <john.mccutchan.wd@gmail.com>
2017-08-07 19:24:51 -0500

    Query successful

---
+++ non_usa_customers.sql
@@ -0,0 +1,10 @@
+/*Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.*/
+
+/*Map*/
+SELECT c.FirstName, c.LastName, c.CustomerId, c.Country
+
+/*What tables*/
+FROM Customer c
+
+/*Filter*/
+WHERE c.Country != 'USA';
