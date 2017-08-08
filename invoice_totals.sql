2fd94633ae5ac5f5d071ff6d2076c5191ebea34f
John McCutchan <john.mccutchan.wd@gmail.com>
2017-08-08 18:39:56 -0500

    Query Works

--- invoice_totals.sql
+++ invoice_totals.sql
@@ -1,2 +1,13 @@
 /*invoice_totals.sql: Provide a query that shows the Invoice Total, Customer name, 
 Country and Sale Agent name for all invoices and customers.*/
+/*Map*/
+SELECT Invoice.Total,(Customer.FirstName || ' ' || Customer.LastName) AS CustomerName, 
+		Customer.Country, (Employee.FirstName || ' ' || Employee.LastName) AS SalesAgent
+
+/*What tables*/
+FROM Invoice 
+	JOIN Customer 
+		ON Customer.CustomerId
+	JOIN Employee 
+		ON Employee.EmployeeId
+
