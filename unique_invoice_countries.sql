550a8ed397a0f1442abc5d1aadfe401e238ad904
John McCutchan <john.mccutchan.wd@gmail.com>
2017-08-07 19:53:13 -0500

    Query Successful

--- unique_invoice_countries.sql
+++ unique_invoice_countries.sql
@@ -1,2 +1,10 @@
 /*unique_invoice_countries.sql: Provide a query showing a unique/distinct 
 list of billing countries from the Invoice table.*/
+
+/*Map*/
+SELECT DISTINCT i.BillingCountry
+
+/*What tables*/
+FROM Invoice i
+
+/*Filter*/
