6e617fab1fb17f5850c38901f17262e42338b44b
John McCutchan <john.mccutchan.wd@gmail.com>
2017-08-08 19:38:19 -0500

    works for one year

--- total_invoices_{year}.sql
+++ total_invoices_{year}.sql
@@ -1,11 +1,10 @@
 /* total_invoices_{year}.sql: How many Invoices were there in 2009 and 2011? */
+
 /*Map*/
-SELECT 
+SELECT count(*) 
 
 /*What tables*/
 FROM Invoice 
 		
-SELECT COALESCE(MAX(id)+1, 0) FROM Invoice
-		
 /*Filter*/
-WHERE Invoice.InvoiceDate BETWEEN '2009-01-01' AND '2009-12-31';
+WHERE Invoice.InvoiceDate BETWEEN '2009-01-01' AND '2009-12-31'
