823740bcf8aced6d47606b58ecb113ecbc734f5e
John McCutchan <john.mccutchan.wd@gmail.com>
2017-08-08 19:20:45 -0500

    in progress

--- total_invoices_{year}.sql
+++ total_invoices_{year}.sql
@@ -1,15 +1,11 @@
 /* total_invoices_{year}.sql: How many Invoices were there in 2009 and 2011? */
 /*Map*/
-SELECT Invoice.Total,(Customer.FirstName || ' ' || Customer.LastName) AS CustomerName, 
-		Customer.Country, (Employee.FirstName || ' ' || Employee.LastName) AS SalesAgent, 
-		Invoice.InvoiceDate
+SELECT 
 
 /*What tables*/
 FROM Invoice 
-	JOIN Customer 
-		ON Customer.CustomerId
-	JOIN Employee 
-		ON Employee.EmployeeId
+		
+SELECT COALESCE(MAX(id)+1, 0) FROM Invoice
 		
 /*Filter*/
-WHERE Invoice.InvoiceDate = '2009-01-01 00:00:00' TO '2009-12-12 00:00:00'
+WHERE Invoice.InvoiceDate BETWEEN '2009-01-01' AND '2009-12-31';
