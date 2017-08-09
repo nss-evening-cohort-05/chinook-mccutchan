2b694d335ebc71e06a10367d318fdf6ba03c9fa4
John McCutchan <john.mccutchan.wd@gmail.com>
2017-08-08 19:51:42 -0500

    query works

--- total_sales_{year}.sql
+++ total_sales_{year}.sql
@@ -1,1 +1,11 @@
 /*total_sales_{year}.sql: What are the respective total sales for each of those years?*/
+
+/*Map*/
+SELECT round(sum(Invoice.Total),2) 
+
+/*What tables*/
+FROM Invoice 
+		
+/*Filter*/
+WHERE Invoice.InvoiceDate BETWEEN '2009-01-01' AND '2009-12-31' 
+		OR Invoice.InvoiceDate BETWEEN'2011-01-01' AND '2011-12-31'
