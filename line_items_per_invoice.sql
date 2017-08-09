375210180c6a49b41d4fae197a52b972fa8b3868
John McCutchan <john.mccutchan.wd@gmail.com>
2017-08-08 20:30:11 -0500

    query works

--- line_items_per_invoice.sql
+++ line_items_per_invoice.sql
@@ -1,2 +1,10 @@
 /*line_items_per_invoice.sql: Looking at the InvoiceLine table, provide a query that 
 COUNTs the number of line items for each Invoice. HINT: GROUP BY*/
+
+SELECT iL.InvoiceId, COUNT(Invoice.InvoiceId) 
+
+FROM InvoiceLine iL
+	JOIN Invoice 
+		ON Invoice.InvoiceId = il.invoiceid
+		
+GROUP BY iL.InvoiceId;
