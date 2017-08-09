3afe235879ec742d3a1a67162350bec6f217ce35
John McCutchan <john.mccutchan.wd@gmail.com>
2017-08-08 20:36:48 -0500

    query works

--- line_item_track.sql
+++ line_item_track.sql
@@ -1,1 +1,8 @@
 /*line_item_track.sql: Provide a query that includes the purchased track name with each invoice line item.*/
+
+SELECT iL.InvoiceId, t.Name
+
+FROM InvoiceLine iL
+	JOIN Track t
+		ON t.TrackId = il.TrackId
+		
