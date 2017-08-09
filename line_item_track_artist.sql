25998075532a81c96e1c03d74fe294dfcad80287
John McCutchan <john.mccutchan.wd@gmail.com>
2017-08-08 20:51:32 -0500

    query works

--- line_item_track_artist.sql
+++ line_item_track_artist.sql
@@ -1,2 +1,12 @@
 /*line_item_track_artist.sql: Provide a query that includes the purchased track name 
 AND artist name with each invoice line item.*/
+
+SELECT iL.InvoiceId, t.Name, aR.Name
+
+FROM InvoiceLine iL
+	JOIN Track t
+		ON t.TrackId = il.TrackId
+	JOIN Album a
+		ON t.AlbumId = a.AlbumId
+	JOIN Artist aR
+		ON aR.ArtistId = a.ArtistId 
